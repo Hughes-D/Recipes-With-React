bb9bcbb208761074cf89b04050f4b28ac57be280
Dave Hughes <76405547+Hughes-D@users.noreply.github.com>
2022-01-17 18:52:37 -0500

    more css

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -10,29 +10,29 @@ export default function Recipe(props) {
             ingredients
     } = props
     return (
-        <div>
-            <div>
-                <h3>{name}</h3>
+        <div className="recipe">
+            <div className="recipe__header">
+                <h3 className='recipe__title'>{name}</h3>
                 <div>
-                    <button>Edit</button>
-                    <button>Delete</button>
+                    <button className='btn btn--primary mr-1'>Edit</button>
+                    <button className='btn btn--danger'>Delete</button>
                 </div>
             </div>
-            <div>
-                <span>Cook Time:</span>
-                <span>{cookTime}</span>
+            <div className='recipe__row'>
+                <span className='recipe__label'>Cook Time:</span>
+                <span className='recipe__value'>{cookTime}</span>
             </div>
-            <div>
-                <span>Servings:</span>
-                <span>{servings}</span>
+            <div className='recipe__row'>
+                <span className='recipe__label'>Servings:</span>
+                <span className='recipe__value'>{servings}</span>
             </div>
-            <div>
-                <span>Instructions:</span>
-                <div>{instructions}</div>
+            <div className='recipe__row'>
+                <span className='recipe__label'>Instructions:</span>
+                <div className='recipe__value recipe__value--indented'>{instructions}</div>
             </div>
-            <div>
-                <span>Ingredients:</span>
-                <div>
+            <div className='recipe__row'>
+                <span className='recipe__label'>Ingredients:</span>
+                <div className='recipe__value recipe__value--indented'>
                     <IngredientList ingredients={ingredients} />
                 </div>
             </div>
