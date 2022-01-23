e6f6f9b86f5a7a71648b5d0afcd77cf0f781d98a
Dave Hughes <76405547+Hughes-D@users.noreply.github.com>
2022-01-23 13:50:35 -0500

    More CSS and finished JSX

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import RecipeList from './RecipeList';
 import '../css/app.css'
 import {v4 as uuidv4} from 'uuid'
+import RecipeEdit from './RecipeEdit';
 
 export const RecipeContext = React.createContext()
 const LOCAL_STORAGE_KEY = 'cookingWithReact.recipes'
@@ -45,6 +46,7 @@ function App() {
   return (
     <RecipeContext.Provider value={recipeContextValue}>
       <RecipeList recipes={recipes} />
+      <RecipeEdit />
     </RecipeContext.Provider>
 
   )
