10f214412733700b0fe79cc47b00eb8e6c5e6a17
Dave Hughes <76405547+Hughes-D@users.noreply.github.com>
2022-06-06 18:55:21 -0400

    bug fix

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -7,6 +7,14 @@ export default function RecipeList({ recipes }) {
     const { handleRecipeAdd } = useContext(RecipeContext)
     return (
         <>
+            <div className="recipe-list__add-recipe-btn-container">
+                <button 
+                    className="btn btn--primary"
+                    onClick={handleRecipeAdd}
+                >
+                    Add Recipe
+                </button>
+                </div>
             <div className="recipe-list">
                 <div>
                     {recipes.map(recipe => {
@@ -15,14 +23,7 @@ export default function RecipeList({ recipes }) {
                         )
                     })}
                 </div>
-                <div className="recipe-list__add-recipe-btn-container">
-                    <button 
-                        className="btn btn--primary"
-                        onClick={handleRecipeAdd}
-                    >
-                        Add Recipe
-                    </button>
-                </div>
+
             </div>
         </>
     )
